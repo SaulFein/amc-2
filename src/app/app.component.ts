ab4ab8f3bee0c7cacbef85cbc34928a087dee9f9
SaulFein <brandon.feinstein@hotmail.com>
2024-04-13 17:16:37 -0700

    nav bar start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'amc-2';
---
+++ src/app/menu-bar/menu-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-menu-bar',
+  standalone: true,
+  imports: [MenubarModule],
+  templateUrl: './menu-bar.component.html',
+  styleUrl: './menu-bar.component.scss',
+})
+export class MenuBarComponent {
+  items = [
+    {
+      label: 'Inventory',
+      items: [
+        {
+          label: 'Current Inventory',
+        },
+        {
+          label: 'Sold',
+        },
+      ],
+    },
+    { label: 'Car Storage' },
+    { label: 'Finance' },
+    { label: 'Contact' },
+  ];
+}
